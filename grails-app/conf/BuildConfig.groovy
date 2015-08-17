03b809f57824e7fcbce82ce5376fb0082478e5ed
Markus List <mlist@health.sdu.dk>
2015-08-17 19:35:24 +0200

    upgrade to grails 2.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,29 +10,14 @@ grails.project.dependency.resolution = {
         grailsCentral()
         mavenLocal()
         mavenCentral()
-		mavenRepo "http://m2repo.spockframework.org/releases/"
-		mavenRepo "http://m2repo.spockframework.org/snapshots/"
     }
     dependencies {
-		test("org.seleniumhq.selenium:selenium-firefox-driver:latest.integration") {
-		    export = false
-		}
-        test "org.spockframework:spock-grails-support:0.7-groovy-2.0"
     }
 	plugins {
-		runtime ":resources:1.0"
+		runtime ":resources:1.2.14"
 		
 		build( ":release:2.2.1") {
 		    export = false
 		}
-		build( ":tomcat:$grailsVersion" ) {
-		    export = false
-		}
-		test( ":geb:0.5.1" ) {
-		    export = false
-		}
-        test(":spock:0.7") {
-            exclude "spock-grails-support"
-        }
 	}
 }
